dd719d709fd316315a02813c3745f8e5ac14c91b
solone313 <solone313@naver.com>
2019-12-22 22:15:30 +0900

    2.1 Reusable Components with JSX + Props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import Potato from './Potato';
 
+function Food({fav}){
+  return <h1>i like {fav}</h1>
+}
 
 function App() {
   return (
     <div>
       <h1>Hello!!!!</h1>
-      <Potato />
+      <Food fav="kimchi" />
+      <Food fav="ramen" />
     </div>
     
   );
