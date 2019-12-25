945769946576eedda97c6c80fc38db9c1aca1a0c
solone313 <solone313@naver.com>
2019-12-25 13:05:59 +0900

    2.4

--- src/App.js
+++ src/App.js
@@ -1,30 +1,43 @@
 import React from 'react';
-
-function Food({name}){
-  return <h1>i like {name}</h1>
+import PropTypes from 'prop-types';
+function Food({name, rating}){
+  return (
+    <div>
+      <h1>i like {name}</h1>
+      <h4>{rating}/5.0</h4>
+    </div>
+  )
 }
 
 const foodLike = [
   {
     id:1,
-    name: "김치"
+    name: "김치",
+    rating: 5
   },
   {
     id:2,
-    name: "삼겹살"
+    name: "삼겹살",
+    rating: 4.7
   },
   {
     id:3,
-    name: "라면"
+    name: "라면",
+    rating: 3
   }
 
 ]
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired
+}
 
 function App() {
   return (
     <div>
       {foodLike.map(dish => 
-        <Food key={dish.id} name={dish.name} />
+        <Food key={dish.id} name={dish.name} rating={dish.rating} />
       )}
     </div>
     
