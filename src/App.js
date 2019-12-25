43bef1b636c89930ea745612d50dad05adff04ed
solone313 <solone313@naver.com>
2019-12-25 14:34:46 +0900

    3.1

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ class App extends React.Component{
     count: 0
   };
   add = () => {
-
+    this.setState(current => ({count : current.count + 1}))
   };
   minus = () => {
-
+    this.setState(current => ({count : current.count - 1}))
   };
   render(){
     return (<div>
