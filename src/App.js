7caf10bd819afd3254863732885b22a29b4db3c6
solone313 <solone313@naver.com>
2019-12-25 12:09:27 +0900

    2.2 ;

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import React from 'react';
 
-function Food({fav}){
-  return <h1>i like {fav}</h1>
+function Food({name}){
+  return <h1>i like {name}</h1>
 }
+
+const foodLike = [
+  {
+    name: "김치"
+  },
+  {
+    name: "삼겹살"
+  },
+  {
+    name: "라면"
+  }
+
+]
 
 function App() {
   return (
     <div>
-      <h1>Hello!!!!</h1>
-      <Food fav="kimchi" />
-      <Food fav="ramen" />
+      {foodLike.map(dish => 
+        <Food name={dish.name} />
+      )}
     </div>
     
   );
