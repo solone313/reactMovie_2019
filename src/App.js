b2210fc9487ef193e1e30bcd635a68aa458ed5d7
solone313 <solone313@naver.com>
2019-12-25 14:52:58 +0900

    3.2

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class App extends React.Component{
+  constructor(props){
+    super(props);
+    console.log("hello");
+  }
   state = {
     count: 0
   };
@@ -11,6 +15,12 @@ class App extends React.Component{
   minus = () => {
     this.setState(current => ({count : current.count - 1}))
   };
+  componentDidMount(){
+    console.log("component rendered");
+  }
+  componentDidUpdate(){
+    console.log("component updated");
+  }
   render(){
     return (<div>
       <h1>The number is: {this.state.count}</h1>
