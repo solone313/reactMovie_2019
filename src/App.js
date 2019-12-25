b6b98574775dae9446d08abc9ed2330636851965
solone313 <solone313@naver.com>
2019-12-25 18:55:06 +0900

    4.0

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import axios from 'axios';
 
 class App extends React.Component{
   state = {
-    isLoading: true
+    isLoading: true,
+    movies: []
+  }
+  getMovies = () => {
+    const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+
   }
   componentDidMount(){
-    setTimeout(() => {
-      this.setState({isLoading: false})
-    }, 6000);
+    this.getMovies();
   }
   render(){
     const { isLoading } = this.state;
